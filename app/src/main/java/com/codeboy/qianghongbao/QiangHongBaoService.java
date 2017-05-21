3403ccd0ad7381353c96427808e7f284aed60161
HP <gxx@163.com>
2017-05-21 23:38:04 +0800

    小秘手机可以成功抢红包

--- app/src/main/java/com/codeboy/qianghongbao/QiangHongBaoService.java
+++ app/src/main/java/com/codeboy/qianghongbao/QiangHongBaoService.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityManager;
@@ -16,69 +17,160 @@ import android.widget.Toast;
 import java.util.List;
 
 /**
- * <p>Created by 李文龙(LeonLee) on 15/2/17 下午10:25.</p>
- * <p><a href="mailto:codeboy2013@163.com">Email:codeboy2013@163.com</a></p>
+ * <p>Created by 卢巧笑(Luqx) on 17/5/20 下午10:25.</p>
  *
  * 抢红包外挂服务
  */
 public class QiangHongBaoService extends AccessibilityService {
-
-    static final String TAG = "QiangHongBao";
-
-    /** 微信的包名*/
-    static final String WECHAT_PACKAGENAME = "com.tencent.mm";
-    /** 红包消息的关键字*/
-    static final String HONGBAO_TEXT_KEY = "[微信红包]";
-
-    Handler handler = new Handler();
     /**
-     * 必须重写的方法：此方法用了接受系统发来的event。在你注册的event发生是被调用。在整个生命周期会被调用多次。
+     * 微信几个页面的包名+地址。用于判断在哪个页面 LAUCHER-微信聊天界面，LUCKEY_MONEY_RECEIVER-点击红包弹出的界面
      */
+    private String LAUCHER = "com.tencent.mm.ui.LauncherUI";
+    private String LUCKEY_MONEY_DETAIL = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI";
+    private String LUCKEY_MONEY_RECEIVER = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI";
+    private String LUCKEY_MONEY_RECEIVER_2 =  "com.tencent.mm.plugin.luckymoney.ui";
+
+
+    /**
+     * 用于判断是否点击过红包了
+     */
+    private boolean isOpenRP;
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        final int eventType = event.getEventType();
+        int eventType = event.getEventType();
+        switch (eventType) {
+            //通知栏来信息，判断是否含有微信红包字样，是的话跳转
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                List<CharSequence> texts = event.getText();
+                for (CharSequence text : texts) {
+                    String content = text.toString();
+                    if (!TextUtils.isEmpty(content)) {
+                        //判断是否含有[微信红包]字样
+                        if (content.contains("[微信红包]")) {
+                            //如果有则打开微信红包页面
+                            openWeChatPage(event);
 
-        Log.d(TAG, "事件---->" + event);
-
-        //通知栏事件
-        if(eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
-            List<CharSequence> texts = event.getText();
-            if(!texts.isEmpty()) {
-                for(CharSequence t : texts) {
-                    String text = String.valueOf(t);
-                    if(text.contains(HONGBAO_TEXT_KEY)) {
-                        openNotify(event);
-                        break;
+                            isOpenRP=false;
+                        }
                     }
                 }
-            }
-        } else if(eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            openHongBao(event);
+                break;
+            //界面跳转的监听
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                String className = event.getClassName().toString();
+                //判断是否是微信聊天界面
+                if (LAUCHER.equals(className)) {
+                    //获取当前聊天页面的根布局
+                    AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+                    //开始找红包
+                    findRedPacket(rootNode);
+                }
+
+                //判断是否是显示‘开’的那个红包界面
+                if (LUCKEY_MONEY_RECEIVER.equals(className)) {
+                    AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+                    //开始抢红包
+                    openRedPacket(rootNode);
+                }
+
+                //判断是否是红包领取后的详情界面
+                if(LUCKEY_MONEY_DETAIL.equals(className)){
+                    //返回桌面
+                    back2Home();
+                }
+                break;
         }
     }
 
-    /*@Override
-    protected boolean onKeyEvent(KeyEvent event) {
-        //return super.onKeyEvent(event);
-        return true;
-    }*/
+    /**
+     * 开始打开红包
+     */
+    private void openRedPacket(AccessibilityNodeInfo rootNode) {
+        for (int i = 0; i < rootNode.getChildCount(); i++) {
+            AccessibilityNodeInfo node = rootNode.getChild(i);
+            if ("android.widget.Button".equals(node.getClassName())) {
+                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+            openRedPacket(node);
+        }
+    }
+
+    /**
+     * 遍历查找红包
+     */
+    private void findRedPacket(AccessibilityNodeInfo rootNode) {
+        if (rootNode != null) {
+            //从最后一行开始找起
+            for (int i = rootNode.getChildCount() - 1; i >= 0; i--) {
+                AccessibilityNodeInfo node = rootNode.getChild(i);
+                //如果node为空则跳过该节点
+                if (node == null) {
+                    continue;
+                }
+                CharSequence text = node.getText();
+                if (text != null && text.toString().equals("领取红包")) {
+                    AccessibilityNodeInfo parent = node.getParent();
+                    //while循环,遍历"领取红包"的各个父布局，直至找到可点击的为止
+                    while (parent != null) {
+                        if (parent.isClickable()) {
+                            //模拟点击
+                            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            //isOpenRP用于判断该红包是否点击过
+                            isOpenRP = true;
+                            break;
+                        }
+                        parent = parent.getParent();
+                    }
+                }
+                //判断是否已经打开过那个最新的红包了，是的话就跳出for循环，不是的话继续遍历
+                if (isOpenRP) {
+                    break;
+                } else {
+                    findRedPacket(node);
+                }
+
+            }
+        }
+    }
+
+    /**
+     * 开启红包所在的聊天页面
+     */
+    private void openWeChatPage(AccessibilityEvent event) {
+        //A instanceof B 用来判断内存中实际对象A是不是B类型，常用于强制转换前的判断
+        if (event.getParcelableData() != null && event.getParcelableData() instanceof Notification) {
+            Notification notification = (Notification) event.getParcelableData();
+            //打开对应的聊天界面
+            PendingIntent pendingIntent = notification.contentIntent;
+            try {
+                pendingIntent.send();
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * 服务连接
+     */
+    @Override
+    protected void onServiceConnected() {
+        Toast.makeText(this, "抢红包服务开启", Toast.LENGTH_SHORT).show();
+        super.onServiceConnected();
+    }
+
     /**
      * 必须重写的方法：系统要中断此service返回的响应时会调用。在整个生命周期会被调用多次。
      */
     @Override
     public void onInterrupt() {
-        Toast.makeText(this, "中断抢红包服务", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我快被终结了啊-----", Toast.LENGTH_SHORT).show();
     }
+
     /**
-     * 服务已连接
-     */
-    @Override
-    protected void onServiceConnected() {
-        super.onServiceConnected();
-        Toast.makeText(this, "连接抢红包服务", Toast.LENGTH_SHORT).show();
-    }
-    /**
-     * 服务已断开
+     * 服务断开
      */
     @Override
     public boolean onUnbind(Intent intent) {
@@ -86,91 +178,14 @@ public class QiangHongBaoService extends AccessibilityService {
         return super.onUnbind(intent);
     }
 
-    private void sendNotifyEvent(){
-        AccessibilityManager manager= (AccessibilityManager)getSystemService(ACCESSIBILITY_SERVICE);
-        if (!manager.isEnabled()) {
-            return;
-        }
-        AccessibilityEvent event=AccessibilityEvent.obtain(AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED);
-        event.setPackageName(WECHAT_PACKAGENAME);
-        event.setClassName(Notification.class.getName());
-        CharSequence tickerText = HONGBAO_TEXT_KEY;
-        event.getText().add(tickerText);
-        manager.sendAccessibilityEvent(event);
-    }
-
-    /** 打开通知栏消息*/
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    private void openNotify(AccessibilityEvent event) {
-        if(event.getParcelableData() == null || !(event.getParcelableData() instanceof Notification)) {
-            return;
-        }
-        //以下是精华，将微信的通知栏消息打开
-        Notification notification = (Notification) event.getParcelableData();
-        PendingIntent pendingIntent = notification.contentIntent;
-        try {
-            pendingIntent.send();
-        } catch (PendingIntent.CanceledException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    private void openHongBao(AccessibilityEvent event) {
-        if("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI".equals(event.getClassName())) {
-            //点中了红包，下一步就是去拆红包
-            checkKey1();
-        } else if("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI".equals(event.getClassName())) {
-            //拆完红包后看详细的纪录界面
-            //nonething
-        } else if("com.tencent.mm.ui.LauncherUI".equals(event.getClassName())) {
-            //在聊天界面,去点中红包
-            checkKey2();
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    private void checkKey1() {
-        Log.i(TAG,"in checkKey1");
-        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        if(nodeInfo == null) {
-            Log.w(TAG, "rootWindow为空");
-            return;
-        }
-        List<AccessibilityNodeInfo> list2 = nodeInfo.findAccessibilityNodeInfosByText("");
-        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("");
-        for(AccessibilityNodeInfo n : list) {
-            n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    private void checkKey2() {
-        Log.i(TAG,"in checkKey2");
-        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        if(nodeInfo == null) {
-            Log.w(TAG, "rootWindow为空");
-            return;
-        }
-        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
-        if(list.isEmpty()) {
-            list = nodeInfo.findAccessibilityNodeInfosByText(HONGBAO_TEXT_KEY);
-            for(AccessibilityNodeInfo n : list) {
-                Log.i(TAG, "-->微信红包:" + n);
-                n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                break;
-            }
-        } else {
-            //最新的红包领起
-            for(int i = list.size() - 1; i >= 0; i --) {
-                AccessibilityNodeInfo parent = list.get(i).getParent();
-                Log.i(TAG, "-->领取红包:" + parent);
-                if(parent != null) {
-                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    break;
-                }
-            }
-        }
+    /**
+     * 返回桌面
+     */
+    private void back2Home() {
+        Intent home=new Intent(Intent.ACTION_MAIN);
+        home.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        home.addCategory(Intent.CATEGORY_HOME);
+        startActivity(home);
     }
 
 }
